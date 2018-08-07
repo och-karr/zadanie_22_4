1cff366e0088cd81e54ad78ce318228602a674d0
karolinneczka <okaa93@gmail.com>
2018-08-07 19:06:29 +0200

    Arrow function generator

--- server/server.js
+++ server/server.js
@@ -5,14 +5,15 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import IntlWrapper from '../client/modules/Intl/IntlWrapper';
 
-// Initialize the Express App
+// Initialize the Express App/ Inicjalizacja aplikacji Express
 const app = new Express();
 
 // Set Development modes checks
 const isDevMode = process.env.NODE_ENV === 'development' || false;
 const isProdMode = process.env.NODE_ENV === 'production' || false;
 
-// Run Webpack dev server in development mode
+//Zależności webpacka
+// Run Webpack dev server in development mode/  Uruchomienie Webpacka w trybie developerskim
 if (isDevMode) {
   // Webpack Requirements
   // eslint-disable-next-line global-require
@@ -118,23 +119,30 @@ const renderError = err => {
 };
 
 // Server Side Rendering based on routes matched by React-router.
+// korzystamy z middleware'a, który przepuszcza każde zapytanie przez funkcję match
 app.use((req, res, next) => {
+  // funkcja match stara się dopasować na podstawie url z którego zostało wykonane zapytanie do jednego z routów
   match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
+    // jeśli podczas dopasowywania routingu pojawił się błąd należy odesłać odpowiedź błędu razem z kodem 500 oznaczającym błąd serwera
     if (err) {
       return res.status(500).end(renderError(err));
     }
 
+    // w przypadku obecności obiektu Location, zostaniemy przekierowani na tę lokalizację. 302 to kod, który oznacza właśnie przekierowanie
     if (redirectLocation) {
       return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
     }
 
+    // renderProps to obiekt, który powinniśmy przekazać jako kontekst dla React Routera. Jeśli go nie ma możemy przepuścić middleware'a dalej
     if (!renderProps) {
       return next();
     }
 
     const store = configureStore();
 
+    // Pobieramy dane potrzebne do "nawodnienia" komponentu.
     return fetchComponentData(store, renderProps.components, renderProps.params)
+      // jeśli nam to wyjdzie korzystamy z metody renderToString zawartej w paczce react-dom/server i na podstawie przekazanych komponentów generujemy kod HTML, który następnie wrzucimy do pełnego szablonu HTML naszej strony.
       .then(() => {
         const initialView = renderToString(
           <Provider store={store}>
@@ -143,13 +151,17 @@ app.use((req, res, next) => {
             </IntlWrapper>
           </Provider>
         );
+
+         // ustalamy ostateczny stan na podstawie wygenerowanego komponentu tak, aby przy starcie aplikacji po stronie klienta można było zasilić store jakimiś danymi.
         const finalState = store.getState();
 
         res
           .set('Content-Type', 'text/html')
           .status(200)
+          // renderujemy całą stronę na podstawie stworzonego pierwszego widoku i stanu apki
           .end(renderFullPage(initialView, finalState));
       })
+      // wyłapujemy wszystkie błędy związane z pobieraniem danych do komponentu
       .catch((error) => next(error));
   });
 });
